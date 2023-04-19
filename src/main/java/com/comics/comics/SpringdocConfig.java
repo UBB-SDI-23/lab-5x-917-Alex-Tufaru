0b4140cdaa731b42cd7b0e7e73badc4eaae86d94
TufaruAlex <35563906+TufaruAlex@users.noreply.github.com>
2023-04-19 20:41:28 +0300

    added swagger

---
+++ src/main/java/com/comics/comics/SpringdocConfig.java
@@ -0,0 +1,16 @@
+package com.comics.comics;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringdocConfig {
+    @Bean
+    public OpenAPI baseOpenApi(){
+        return new OpenAPI().info(new Info().title("API Documentation").version("1.0.0").description("API Documentation"));
+    }
+}
