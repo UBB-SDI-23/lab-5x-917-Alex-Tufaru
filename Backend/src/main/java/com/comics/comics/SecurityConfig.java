40c11150c71de2a0e6b0610a4558a06f24ad732a
TufaruAlex <91742421+9x7-Alex-Tufaru@users.noreply.github.com>
2023-05-07 22:21:21 +0300

    changed backend url

--- Backend/src/main/java/com/comics/comics/SecurityConfig.java
+++ Backend/src/main/java/com/comics/comics/SecurityConfig.java
@@ -1,21 +1,21 @@
-//package com.comics.comics;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .requiresChannel(channel ->
-//                        channel.anyRequest().requiresSecure())
-//                .authorizeRequests(authorize ->
-//                        authorize.anyRequest().permitAll())
-//                .build();
-//    }
-//}
+package com.comics.comics;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize ->
+                        authorize.anyRequest().permitAll())
+                .build();
+    }
+}
--- react-frontend-comics/src/constants.ts
+++ react-frontend-comics/src/constants.ts
@@ -1,5 +1,5 @@
-const PROD_BACKEND_API_URL = "http://34.88.117.38:80/api";
-const DEV_BACKEND_API_URL = "http://127.0.0.1:80/api";
+const PROD_BACKEND_API_URL = "https://34.88.117.38:80/api";
+const DEV_BACKEND_API_URL = "https://127.0.0.1:80/api";
 
 export const BACKEND_API_URL =
     process.env.NODE_ENV === "development"
