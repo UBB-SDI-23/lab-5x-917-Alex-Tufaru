749c91f7035e571fea1ae4e07384878cb531f774
TufaruAlex <91742421+9x7-Alex-Tufaru@users.noreply.github.com>
2023-05-08 12:09:04 +0300

    changed backend url

--- react-frontend-comics/src/constants.ts
+++ react-frontend-comics/src/constants.ts
@@ -1,4 +1,4 @@
-const PROD_BACKEND_API_URL = "https://34.88.117.38:80/api";
+const PROD_BACKEND_API_URL = "/api";
 const DEV_BACKEND_API_URL = "https://127.0.0.1:80/api";
 
 export const BACKEND_API_URL =
